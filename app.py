a2610a3aacfd1c1f037654f18da7573216936b47
A-Ethan <ethan.shen@ucloud.cn>
2020-05-19 21:26:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 @app.route('/')
 
 def hello():
-    html = "<h3>Hello {name}!</h3>" \
+    html = "<h3>Hello Kubernetes! i'm {name}! </h3>" \
            "<b>主机名:</b> {hostname}<br/>"
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+    return html.format(name=os.getenv("NAME", "jenkins"), hostname=socket.gethostname())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
